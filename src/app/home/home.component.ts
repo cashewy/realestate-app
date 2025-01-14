e0bcbb97ff42bc105c36ee93d497a7ba3db25067
cashewy <hewchristian@gmail.com>
2025-01-14 11:52:30 -0500

    initial commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+@Component({
+  selector: 'app-home',
+  imports: [CommonModule,HousingLocationComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
